fc86ada5d30a8d1f2e4ce60547831770143bd9ee
daylightcoderj6 <jananibharathi6@gmail.com>
2024-02-15 13:03:40 +0530

    added region.js

---
+++ day7/region.js
@@ -0,0 +1,10 @@
+let XMLHttpRequest = require ('xhr2');
+let xhr = new XMLHttpRequest();
+xhr.open('GET','https://restcountries.com/v3.1/all');
+xhr.send();
+xhr.onload = function() {
+    let countries = JSON.parse(xhr.responseText);
+ 
+  const country = countries.filter((element)=>element.region === "Asia")
+  console.log(country.map((element)=>element.name.common))
+    }
